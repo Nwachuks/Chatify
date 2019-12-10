523e528ffbd257814c174a59a98dcd3c7183a4f5
Nwachuks <nwachukwu.ejiofor@hotmail.com>
2019-12-10 09:11:39 +0100

    Initial Commit

---
+++ Chatify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatify
+//
+//  Created by Coded Professor on 10/12/2019.
+//  Copyright © 2019 Coded Professor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
