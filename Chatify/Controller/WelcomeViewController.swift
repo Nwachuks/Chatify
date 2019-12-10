9ac4bbbd83456c9a74d1ed5cece30d41921bc896
Nwachuks <nwachukwu.ejiofor@hotmail.com>
2019-12-10 16:06:12 +0100

    Set up navigation

--- Chatify/Controller/WelcomeViewController.swift
+++ Chatify/Controller/WelcomeViewController.swift
@@ -15,6 +15,17 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        titleLabel.text = ""
+        
+        var charIndex = 0.0
+        let titleText = "⚡️Chatify"
+        
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
 
 
